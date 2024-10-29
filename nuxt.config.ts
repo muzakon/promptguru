d8679e26a394eaa46a7240099e8c93b33d206ffd
muzakon <hasanmuzak@hotmail.com>
2024-10-29 13:06:03 +0300

    Update: Homepage Slider Fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@pinia/nuxt',
-    '@nuxtjs/tailwindcss',
-    'dayjs-nuxt',
-    'nuxt-lodash',
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+    "dayjs-nuxt",
+    "nuxt-lodash",
   ],
-  plugins: [
-    "@/plugins/antd"
-  ],
+  plugins: ["@/plugins/antd"],
   css: ["@/assets/style/main.css", "@egjs/vue3-flicking/dist/flicking.css"],
   tailwindcss: {
     config: {
       theme: {
         screens: {
-          'xs': '0px',
-          'sm': '576px',
+          sm: "576px",
           // => @media (min-width: 640px) { ... }
-    
-          'md': '768px',
+
+          md: "768px",
           // => @media (min-width: 768px) { ... }
-    
-          'lg': '992x',
+
+          lg: "992x",
           // => @media (min-width: 1024px) { ... }
-    
-          'xl': '1200px',
+
+          xl: "1200px",
           // => @media (min-width: 1280px) { ... }
-    
-          '2xl': '1400px',
+
+          "2xl": "1400px",
           // => @media (min-width: 1536px) { ... }
-        }
-    
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  },
+});
--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,18 @@
           >
             <div
               class="panel w-full rounded-[5px] h-[440px] bg-center bg-no-repeat bg-cover relative sliderCover"
+              v-for="(slider, index) in sliderItems"
+              :key="index"
             >
               <div
                 class="absolute w-full h-full left-0 top-0 flex items-center flex-col justify-center p-4 text-white z-[5]"
               >
                 <div class="text-[16px] font-bold text-left w-full uppercase">
-                  {{ sliderItems[slider].title }}
+                  asd
                 </div>
 
                 <div class="text-[52px] font-black text-left w-full my-[12px]">
-                  {{ sliderItems[slider].title }}
+                  {{ slider.title }}
                 </div>
 
                 <div class="leading-[22px] text-left w-full">
@@ -88,13 +90,44 @@
           </div>
         </div>
       </div>
+
+      <div class="mt-[24px] w-full">
+        <span
+          class="inline-block bg-blue-400/10 text-[12px] text-blue-300 hover:text-white hover:bg-blue-400/20 mr-2 px-4 py-2 rounded-full cursor-pointer mb-[8px]"
+          v-for="(category, index) in generativeAiImageCategories.slice(0, 15)"
+          :key="index"
+          >{{ category }}</span
+        >
+
+        <div class="mt-[12px] flex items-center justify-center">
+          <div
+            class="inline-block cursor-pointer text-center hover:text-white text-white/50 uppercase text-[12px] font-semibold"
+          >
+            <div class="flex items-center group">
+              <span class="transition-all duration-200 group-hover:mr-2.5 mr-2">
+                Browse All
+              </span>
+              <Icon
+                name="icon-park-outline:arrow-right"
+                class="transition-all duration-200"
+                :size="20"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="bg-neutral-900 p-4">
+          <h3 class="uppercase font-semibold">Bests of the month</h3>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Flicking from "@egjs/vue3-flicking";
-const slider: any = ref(0);
+const slider: any = ref(null);
+const activeSliderIndex: any = ref(0);
 
 const sliderItems = ref([
   {
@@ -137,30 +170,159 @@ const sliderItems = ref([
 
 const sliderItemInterval = ref(null);
 
+const generativeAiImageCategories = ref([
+  "Food",
+  "Marketing",
+  "Environment",
+  "Furniture",
+  "Character Design",
+  "Fashion",
+  "Architecture",
+  "Healthcare",
+  "Travel",
+  "Gaming",
+  "Education",
+  "Art",
+  "Sports",
+  "Technology",
+  "Entertainment",
+  "Product Design",
+  "Interior Design",
+  "Nature",
+  "Transportation",
+  "Finance",
+  "Social Media",
+  "Photography",
+  "Virtual Reality",
+  "Augmented Reality",
+  "Web Design",
+  "Graphic Design",
+  "E-commerce",
+  "Events",
+  "Branding",
+  "Music",
+  "Film",
+  "Animation",
+  "Lifestyle",
+  "Beauty",
+  "Animals",
+  "Illustration",
+  "Public Relations",
+  "Real Estate",
+  "Retail",
+  "Home Decor",
+  "Culinary Arts",
+  "Science",
+  "Historical Recreation",
+  "Sportswear",
+  "Toys",
+  "Jewelry",
+  "Crafts",
+  "Wellness",
+  "Fitness",
+  "DIY Projects",
+  "Children's Books",
+  "Comics",
+  "Cultural Heritage",
+  "Social Impact",
+  "Fictional Worlds",
+  "Fantasy",
+  "Mythology",
+  "Digital Collectibles",
+  "Urban Planning",
+  "Nature Conservation",
+  "Sustainability",
+  "Smart Cities",
+  "Data Visualization",
+  "User Experience Design",
+  "Corporate Training",
+  "Non-profit Campaigns",
+  "Public Safety",
+  "Space Exploration",
+  "Weather Patterns",
+  "Political Campaigns",
+  "Crisis Management",
+  "Virtual Events",
+  "Online Education",
+  "Community Engagement",
+  "Mental Health",
+  "Personal Branding",
+  "Cybersecurity",
+  "Historical Documentaries",
+  "Pet Care",
+  "Gardening",
+  "Home Renovation",
+  "Personal Finance",
+  "Travel Guides",
+  "Language Learning",
+  "Crisis Communications",
+  "Disaster Relief",
+  "Social Entrepreneurship",
+  "Cultural Festivals",
+  "Seasonal Promotions",
+  "Trend Analysis",
+  "Sports Analysis",
+  "Influencer Marketing",
+  "Virtual Influencers",
+  "Augmented Learning",
+  "Bioengineering",
+  "Ecological Studies",
+  "Anthropology",
+  "Philosophy",
+  "Psychology",
+  "Personal Growth",
+  "Retreats",
+  "Volunteer Work",
+  "Adventure Tourism",
+  "Local Experiences",
+  "Home Office Design",
+  "Pet Fashion",
+  "Health and Nutrition",
+  "Retirement Planning",
+  "Elder Care",
+  "Hobbies",
+  "Tech Gadgets",
+  "Smart Home Devices",
+  "Eco-friendly Products",
+  "Luxury Lifestyle",
+  "Culinary Tourism",
+  "Cultural Education",
+  "Historical Preservation",
+  "Online Communities",
+  "Futurism",
+  "Metaverse",
+  "Digital Art",
+  "Mixed Media",
+  "Community Arts",
+  "Social Movements",
+  "Activism",
+  "Environmental Justice",
+  "Ethical Fashion",
+  "Sustainable Living",
+  "Green Technologies",
+  "Digital Nomad Lifestyle",
+]);
+
 function setSliderItemInterval() {
   if (!sliderItemInterval.value) {
     setInterval(() => {
       const activeItemIndex = sliderItems.value.findIndex(
         (slider) => slider.active
       );
-      const nextItemIndex =
+
+      activeSliderIndex.value =
         activeItemIndex === sliderItems.value.length - 1
           ? 0
           : activeItemIndex + 1;
 
       sliderItems.value[activeItemIndex].active = false;
-      sliderItems.value[nextItemIndex].active = true;
-
-      slider.value.moveTo(nextItemIndex);
+      sliderItems.value[activeSliderIndex.value].active = true;
+      slider.value.moveTo(activeSliderIndex.value);
     }, 5000);
   }
 }
 
 onMounted(() => {
-  if (slider.value) {
-    slider.value.moveTo(2);
-  }
-
   setSliderItemInterval();
 });
 
